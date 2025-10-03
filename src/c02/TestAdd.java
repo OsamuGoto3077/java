17d4a86099fca35ab0e5c237954a9c28cce0ede6
OsamuGoto <osagoto@sea.plala.or.jp>
2025-10-03 11:41:25 +0900

    新規作成

---
+++ src/c02/TestAdd.java
@@ -0,0 +1,13 @@
+package c02;
+
+public class TestAdd {
+
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 0; i <= 9; i++) {
+			sum += i;
+		}
+		System.out.println("1から9の合計は：" + sum);
+	}
+
+}
